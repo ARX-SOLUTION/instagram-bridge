f1a26b14538833519aa32d99e96935c0bcc6de04
Xamidullo Xudoyberdiyev <khkhamidullo@gmail.com>
2026-02-22 16:29:34 +0500

    Refactor webhook send-message to reuse Telegram service

--- src/instagram/instagram.module.ts
+++ src/instagram/instagram.module.ts
@@ -5,12 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 import { InstagramController } from './instagram.controller';
 import { InstagramService } from './instagram.service';
 import { InstagramPost } from './entities/instagram-post.entity';
+import { TelegramModule } from '../telegram/telegram.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([InstagramPost]),
     HttpModule,
     ConfigModule,
+    TelegramModule,
   ],
   controllers: [InstagramController],
   providers: [InstagramService],
